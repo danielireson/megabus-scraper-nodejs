8055e3175f6cda944185e9bee2b819dfb4e30ef8
Daniel Ireson <daniel@djgraphics.co.uk>
2017-01-23 15:10:46 +0000

    add microservice scaffold

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-console.log('index.js')
+const {send} = require('micro')
+
+module.exports = async function (request, response) {
+  let json = {
+    code: 200,
+    message: 'There were X results',
+    data: []
+  }
+  
+  send(response, 200, json)
+}
