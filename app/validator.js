0ba2899826e4438e98362df40cc8bbe666cc7ea9
Daniel Ireson <daniel@djgraphics.co.uk>
2017-01-23 17:00:29 +0000

    change speech marks to quotation marks in validator

--- app/validator.js
+++ app/validator.js
@@ -13,19 +13,19 @@ module.exports = function(originLocation, destinationLocation, startDate, endDat
 
 function checkRequiredParams(originLocation, destinationLocation, startDate, endDate) {
   if (originLocation == null) {
-    throw createError(400, "Origin location was not provided")
+    throw createError(400, 'Origin location was not provided')
   }
 
   if (destinationLocation == null) {
-    throw createError(400, "Destination location was not provided")
+    throw createError(400, 'Destination location was not provided')
   }
 
   if (startDate == null) {
-    throw createError(400, "Start date was not provided")
+    throw createError(400, 'Start date was not provided')
   }
 
   if (endDate == null) {
-    throw createError(400, "End date was not provided")
+    throw createError(400, 'End date was not provided')
   }
 }
 
