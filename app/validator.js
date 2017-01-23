57b97f7410b9f9322f3b7d5fe40c6bb2537f5bd3
Daniel Ireson <daniel@djgraphics.co.uk>
2017-01-23 16:51:43 +0000

    add valid date validation

--- app/validator.js
+++ app/validator.js
@@ -1,10 +1,13 @@
 const {createError} = require('micro')
+const moment = require('moment')
 const locations = require('./data/locations.json')
 
 module.exports = function(originLocation, destinationLocation, startDate, endDate) {
   checkRequiredParams(originLocation, destinationLocation, startDate, endDate)
   checkValidLocation(originLocation)
   checkValidLocation(destinationLocation)
+  checkValidDate(startDate)
+  checkValidDate(endDate)
 }
 
 function checkRequiredParams(originLocation, destinationLocation, startDate, endDate) {
@@ -28,5 +31,11 @@ function checkRequiredParams(originLocation, destinationLocation, startDate, end
 function checkValidLocation(location) {
   if (!(location in locations)) {
     throw createError(400, `${location} is not a valid location`)
+  }
+}
+
+function checkValidDate(date) {
+  if (!moment(date, 'DD-MM-YYYY').isValid()) {
+    throw createError(400, `${date} is not a valid date string, dates should be formatted as 'dd-mm-yyyy'`)
   }
 }
