e0ec089dd87c72f1c3b3dd1228f80d66de936320
Daniel Ireson <daniel@djgraphics.co.uk>
2017-01-23 19:11:51 +0000

    move request format comment to outside function

--- app/index.js
+++ app/index.js
@@ -2,8 +2,8 @@ const url = require('url')
 const {send} = require('micro')
 const validator = require('./validator')
 
+// Request format == /:originLocation/:desinationLocation/:startDate/:endDate
 module.exports = async function (request, response) {
-  // :originLocation/:desinationLocation/:startDate/:endDate
   const params = url.parse(request.url).pathname.split('/').splice(1)
   validator(...params)
 
