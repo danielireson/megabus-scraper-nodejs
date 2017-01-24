43c52e8f82fef9eb5b6dda41949258315bb8eb91
Daniel Ireson <daniel@djgraphics.co.uk>
2017-01-24 15:40:44 +0000

    add timestmap console log on each new server request

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
 const url = require('url')
 const dotenv = require('dotenv').config()
 const {send} = require('micro')
+const moment = require('moment')
 const authenticator = require('./authenticator')
 const validator = require('./validator')
 const scraper = require('./scraper')
 
 // Request format == /:originLocation/:desinationLocation/:startDate/:endDate
 module.exports = async function (request, response) {
+  console.log(moment().format('HH:mm') + ' Received request')
+
   authenticator(request)
 
   const params = url.parse(request.url).pathname.split('/').splice(1)
