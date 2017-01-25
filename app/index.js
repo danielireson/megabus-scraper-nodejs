87af959c6de6b38cc0f7d5adce3a4956ea3691c0
Daniel Ireson <daniel@djgraphics.co.uk>
2017-01-25 15:35:09 +0000

    throw errors as json

--- app/index.js
+++ app/index.js
@@ -11,20 +11,35 @@ const analyser = require('./analyser')
 module.exports = async function (request, response) {
   console.log(moment().format('HH:mm') + ' Received request')
 
-  authenticator(request)
+  try {
+    authenticator(request)
 
-  const params = url.parse(request.url).pathname.split('/').splice(1)
-  validator(...params)
+    const params = url.parse(request.url).pathname.split('/').splice(1)
+    validator(...params)
 
-  let results = await scraper(...params)
+    let results = await scraper(...params)
+    sendResultsAsJson(response, results)
+
+  } catch (error) {
+    sendErrorAsJson(response, error)
+  }
+}
+
+function sendErrorAsJson(response, error) {
+  let json = {
+    code: error.statusCode,
+    message: error.message
+  }
+  send(response, error.statusCode, json)
+}
+
+function sendResultsAsJson(response, results) {
   let analysis = analyser(results)
-
   let json = {
     code: 200,
     message: `There are ${analysis.noOfJourneys} journeys`,
     stats: analysis,
     data: results
   }
-
   send(response, 200, json)
 }
