589ee1a84fe526e8f0f05995db83cb122a0fee84
Daniel Ireson <daniel@djgraphics.co.uk>
2017-01-30 14:49:43 +0000

    fix: only run auth on non-options requests

--- app/http/authenticator.js
+++ app/http/authenticator.js
@@ -2,16 +2,18 @@ const {createError} = require('micro')
 const responder = require('../http/responder')
 
 module.exports = function(request, response) {
-  const userKey = request.headers['x-authorization']
-  const apiKey = process.env.API_KEY || 'megabus-scraper-nodejs'
+  if (request.method !== 'OPTIONS') {
+    const userKey = request.headers['x-authorization']
+    const apiKey = process.env.API_KEY || 'megabus-scraper-nodejs'
 
-  if (userKey == null) {
-    let error = createError(401, 'Please set the X-Authorization header with a valid API key')
-    responder.sendErrorAsJson(response, error)
-  }
+    if (userKey == null) {
+      let error = createError(401, 'Please set the X-Authorization header with a valid API key')
+      responder.sendErrorAsJson(response, error)
+    }
 
-  if (userKey !== apiKey) {
-    let error = createError(401, 'Invalid API key passed as X-Authorization header')
-    responder.sendErrorAsJson(response, error)
+    if (userKey !== apiKey) {
+      let error = createError(401, 'Invalid API key passed as X-Authorization header')
+      responder.sendErrorAsJson(response, error)
+    }
   }
 }
