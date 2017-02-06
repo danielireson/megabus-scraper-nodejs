a3f0ae115af97a6795b0aae6729c47e0f97b74b5
Daniel Ireson <daniel@djgraphics.co.uk>
2017-02-06 20:31:57 +0000

    setup search test structure

---
+++ test/search.test.js
@@ -0,0 +1,27 @@
+require('async-to-gen/register')
+require('dotenv').config()
+const micro = require('micro')
+const test = require('ava')
+const listen = require('test-listen')
+const request = require('request-promise')
+const app = require('../app')
+
+function buildRequestOptions(...routes) {
+  let url = routes.reduce((concat, route) => concat += '/' + route)
+  return {
+    uri: url,
+    headers: {
+      'X-Authorization': process.env.API_KEY
+    },
+    json: true
+  }
+}
+
+test.beforeEach(async t => {
+  t.context.url = await listen(micro(app))
+})
+
+test('search: failure', async t => {
+  let options = buildRequestOptions(t.context.url, 'search')
+  const body = await request(options) 
+})
