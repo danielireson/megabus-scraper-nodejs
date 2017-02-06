402fcf2f12db2a51d6c6f109c2752ba1e6973e7d
Daniel Ireson <daniel@djgraphics.co.uk>
2017-02-06 21:47:18 +0000

    add search date failure tests

--- test/search.test.js
+++ test/search.test.js
@@ -46,3 +46,43 @@ test('search: no search start date', async t => {
   t.is(result.error.code, 400)
   t.is(result.error.message, 'Start date was not provided')
 })
+
+test('search: order of dates', async t => {
+  let today = moment().format('DD-MM-YYYY')
+  let tomorrow = moment().add(1, 'days').format('DD-MM-YYYY')
+  let options = buildRequestOptions(t.context.url, 'search', 'manchester', 'london', tomorrow, today)
+  let result = await t.throws(request(options))
+  t.is(result.error.code, 400)
+  t.is(result.error.message, 'The provided search start date is after the search end date')
+})
+
+test('search: invalid search start date', async t => {
+  let options = buildRequestOptions(t.context.url, 'search', 'manchester', 'london', 'start-date-invalid')
+  let result = await t.throws(request(options))
+  t.is(result.error.code, 400)
+  t.is(result.error.message, "start-date-invalid is not a valid date string, dates should be formatted as 'dd-mm-yyyy'")
+})
+
+test('search: invalid search start date', async t => {
+  let options = buildRequestOptions(t.context.url, 'search', 'manchester', 'london', 'end-date-invalid')
+  let result = await t.throws(request(options))
+  t.is(result.error.code, 400)
+  t.is(result.error.message, "end-date-invalid is not a valid date string, dates should be formatted as 'dd-mm-yyyy'")
+})
+
+test('search: dates not within one month', async t => {
+  let tomorrow = moment().add(1, 'days').format('DD-MM-YYYY')
+  let tomorrowPlusTwoMonth = moment().add(60, 'days').format('DD-MM-YYYY')
+  let options = buildRequestOptions(t.context.url, 'search', 'manchester', 'london', tomorrow, tomorrowPlusTwoMonth)
+  let result = await t.throws(request(options))
+  t.is(result.error.code, 400)
+  t.is(result.error.message, 'Search dates must be within one month of each other')
+})
+
+test('search: date future check', async t => {
+  let today = moment().format('DD-MM-YYYY')
+  let options = buildRequestOptions(t.context.url, 'search', 'manchester', 'london', today)
+  let result = await t.throws(request(options))
+  t.is(result.error.code, 400)
+  t.is(result.error.message, 'Search dates must be in the future')
+})
